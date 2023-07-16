da0bfd7226870295344b483c60e0f721cb50ebb1
meitrix8208 <maurrod2001@outlook.com>
2023-07-16 12:54:55 -0500

    first implementation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![allow(unused)]
 use clap::Parser;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
 #[derive(Parser, Debug)]
 struct Cli {
     pattern: String,
@@ -8,5 +11,20 @@ struct Cli {
 
 fn main() {
     let args = Cli::parse();
-    println!("args: {:?}", args);
+    let mut content = BufReader::new(File::open(&args.path).unwrap());
+    // let mut buffer = String::new();
+    // content.read_to_string(&mut buffer).unwrap();
+    // let mut lines = buffer.lines();
+    // let mut line = lines.next();
+    // while line != None {
+    //     if line.as_ref().unwrap().contains(&args.pattern) {
+    //         println!("{:?}", line.unwrap().trim());
+    //     }
+    //     line = lines.next();
+    // }
+    for line in content.lines() {
+        if line.as_ref().unwrap().contains(&args.pattern) {
+            println!("{:?}", line.unwrap().trim());
+        }
+    }
 }
