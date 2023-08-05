b85d9defde6bd9b6299cc490ed74230aea88ae38
meitrix8208 <maurrod2001@outlook.com>
2023-08-05 11:51:20 -0500

    update implementation

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,63 @@
-#![allow(unused)]
 use clap::Parser;
 use console::style;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
+use regex::Regex;
+
 #[derive(Parser, Debug)]
 struct Cli {
     pattern: String,
     path: std::path::PathBuf,
-}
 
-fn some_kind_of_uppercase_first_letter(s: &str) -> String {
-    let mut c = s.chars();
-    match c.next() {
-        None => String::new(),
-        Some(f) => f.to_uppercase().collect::<String>() + c.as_str(),
-    }
+    #[clap(short, long)]
+    insensitive: bool,
+
+    #[clap(short, long)]
+    exact: bool,
 }
 
 fn main() {
     let args = Cli::parse();
+
     let file = File::open(&args.path);
     let mut count = 1;
+
     if file.is_err() {
         println!("Error reading file, {}", args.path.display());
         return;
+    }
+
+    let content = BufReader::new(file.unwrap());
+    let regex_pattern = if args.insensitive {
+        format!("(?i){}", regex::escape(&args.pattern))
     } else {
-        // lee el archivo
-        let mut content = BufReader::new(file.unwrap());
-        // lee el contenido del archivo
-        let mut buffer: String = String::new();
+        regex::escape(&args.pattern)
+    };
+    let regex = Regex::new(&regex_pattern).unwrap();
 
-        content.read_to_string(&mut buffer).unwrap();
+    for line in content.lines() {
+        let line = line.unwrap();
+        let str_count = style(count).cyan().bold().to_string();
+        let mut colored_line = line.to_string();
 
-        let mut lines = buffer.lines();
-        let mut line = lines.next();
-        let to_lower_case = args.pattern.as_str().to_lowercase();
-        let to_upper_case = some_kind_of_uppercase_first_letter(&args.pattern);
-        while line != None {
-            // incluir también palabras que contengan el patron incluso si están capitalizadas
-            let str_count = style(count).cyan().bold().to_string();
-            if line.as_ref().unwrap().contains(&to_lower_case) {
-                println!(
-                    "{str_count}: {}",
-                    line.unwrap().trim().replace(
-                        &to_lower_case,
-                        &style(&to_lower_case.trim()).green().bold().to_string()
-                    )
-                );
-            } else if line.as_ref().unwrap().contains(&to_upper_case) {
-                println!(
-                    "{str_count}: {}",
-                    line.unwrap().trim().replace(
-                        &to_upper_case,
-                        &style(&to_upper_case.trim()).red().bold().to_string().trim()
-                    )
-                );
-            }
-            line = lines.next();
-            count += 1;
+        for captures in regex.captures_iter(&line) {
+            let pattern_match = captures.get(0).unwrap().as_str();
+            let colored_pattern = if args.exact && line == pattern_match {
+                style(pattern_match).blue().bold()
+            } else if args.insensitive && line.to_lowercase() == pattern_match.to_lowercase() {
+                style(pattern_match).green().bold()
+            } else if args.insensitive {
+                style(pattern_match).yellow().bold()
+            } else {
+                style(pattern_match).red().bold()
+            };
+            colored_line = colored_line.replace(pattern_match, &colored_pattern.to_string());
         }
-        buffer.clear();
+
+        if colored_line != line {
+            println!("{str_count}: {colored_line}", str_count = str_count, colored_line = colored_line);
+        }
+        count += 1;
     }
 }
